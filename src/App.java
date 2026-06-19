748bcde97950142cef377f1e6fbb46e194ed9675
Ahmed Ayman Khalil <ahmedaymen10111@gmail.com>
2026-06-19 20:41:34 +0300

    v2 edit in class car make that abstract class and abstract display info + create class Car

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
 
-    static class Car{
+    abstract static class Car{
         protected String brand;
         protected String model;
         protected short year;
@@ -19,8 +19,27 @@ public class App {
         void stopCar(String brand, String model){
             System.out.println("car is Stoping: "+ brand + " " + model);
         }
-        void displayCarInforamtion(String brand, String model, short year, float cloack, byte day, byte month, short yearNow){
-            System.out.println("car: "+ brand + " " + model + " "+ year + "\n time now: "+ cloack + " / day: "+ day + "-" + month + "-" + yearNow);
+        abstract void displayCarInforamtion(String brand, String model, short year, float cloack, byte day, byte month,short yearNow);
+    }
+    static class SportsCar extends Car{
+        private short maxSpeed;
+        SportsCar(String brand, String model, short year, short maxSpeed){
+            super(brand, model, year);
+            this.maxSpeed = maxSpeed;
+        }
+        void showMaxSpeed(){
+            System.out.println(maxSpeed);
+        }
+        @Override
+        void displayCarInforamtion(String brand, String model, short year, float cloack, byte day, byte month,short yearNow){
+            System.out.print("Brand: " + brand);
+            System.out.print(" Model: " + model);
+            System.out.print(" Year: " + year);
+            System.out.println("Max Speed: " + maxSpeed);
+            System.out.print("Clock: " + cloack);
+            System.out.print(" - Day: " + day);
+            System.out.print(" - Month: " + month);
+            System.out.println(" - YearNow: " + yearNow);
         }
     }
     
